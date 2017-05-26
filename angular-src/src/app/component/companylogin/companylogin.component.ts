ec21f2c30780dd7c7ceeb1e6862c856a590d5f1f
rihazzahir <rihazzahir@gmail.com>
2017-05-26 20:34:14 +0530

    company login authentication and jwt storage in localStorage done!

--- angular-src/src/app/component/companylogin/companylogin.component.ts
+++ angular-src/src/app/component/companylogin/companylogin.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyauthService } from '../../services/companyauth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
-  selector: 'app-companylogin',
-  templateUrl: './companylogin.component.html',
-  styleUrls: ['./companylogin.component.css']
+	selector: 'app-companylogin',
+	templateUrl: './companylogin.component.html',
+	styleUrls: ['./companylogin.component.css']
 })
 export class CompanyloginComponent implements OnInit {
+	email: String;
+	password: String;
 
-  constructor() { }
+  	constructor(
+  		private companyauthService: CompanyauthService,
+  		private router: Router,
+  		private flashMessage: FlashMessagesService
+  	){}
 
-  ngOnInit() {
-  }
+	ngOnInit(){
+	}
+
+	onCompanyLogin(){
+		const company = {
+			email: this.email,
+			password: this.password
+		}
+		console.log(company);
+
+		// Authenticate
+		this.companyauthService.authenticateCompany(company).subscribe(data => {
+			console.log(data);
+			
+			if(data.success){
+				this.companyauthService.storeCompanyData(data.company, data.jwt);
+				this.flashMessage.show('login successful', {cssClass: 'alert-success', timeout: 3000});
+				this.router.navigate(['/company/dashboard']);
+			}
+			else{
+				this.flashMessage.show(data.msg, {cssClass: 'alert-success', timeout: 3000});
+				this.router.navigate(['/company/login']);
+			}
+		});
+	}
 
 }
--- angular-src/src/app/services/companyauth.service.ts
+++ angular-src/src/app/services/companyauth.service.ts
@@ -11,9 +11,26 @@ export class CompanyauthService {
 	  	private http: Http
   	){}
 
+	// Register new Company
 	registerCompany(company){
 		let headers = new Headers();
 		headers.append('Content-Type', 'application/json');
 		return this.http.post('http://localhost:5000/company/register', company, {headers: headers}).map(res => res.json());
+	}
+
+	// Authenticate login for existing user
+	authenticateCompany(company){
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		return this.http.post('http://localhost:5000/company/authenticate', company, {headers: headers}).map(res => res.json());
+	}
+
+	// Store jwt value in local Storage
+	storeCompanyData(company, token){
+		localStorage.setItem('id_token', token);
+		localStorage.setItem('user', JSON.stringify(company)); // company and student both will be 
+		// stored as user as we ccan't determine what type of user it is before asking for user var
+		this.authToken = token;
+		this.company = company;
 	}
 }
